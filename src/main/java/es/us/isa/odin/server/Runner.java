0bbc7954edc262abbdf760f218d189c4520c0ad1
Highfredo <highfredo@gmail.com>
2014-10-12 21:03:41 +0200

    preparado para desplegar en heroku una prueba

--- src/main/java/es/us/isa/odin/server/Runner.java
+++ src/main/java/es/us/isa/odin/server/Runner.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 
 public class Runner {
     public static void main(String[] args) {
+    	String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        System.setProperty("server.port", webPort);
         SpringApplication.run(AppConfiguration.class);
     }
 }
--- src/main/java/es/us/isa/odin/server/controllers/IndexController.java
+++ src/main/java/es/us/isa/odin/server/controllers/IndexController.java
@@ -1,13 +1,11 @@
 package es.us.isa.odin.server.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@Controller
+//@Controller
 public class IndexController {
 
-
+	/*
 	@RequestMapping("/")
 	public String view() {
 		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
@@ -18,6 +16,6 @@ public class IndexController {
 	public String sample() {
 		System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
 		return "index";
-	}
+	}*/
 	
 }
